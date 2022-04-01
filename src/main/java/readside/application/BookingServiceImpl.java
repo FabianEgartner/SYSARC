bcb432f65f08863fe38ad006539905567d930874
fabian_egartner <fabian.egartner@hotmail.com>
2022-04-01 09:00:37 +0200

    readside

--- src/main/java/readside/application/BookingServiceImpl.java
+++ src/main/java/readside/application/BookingServiceImpl.java
@@ -1,15 +1,24 @@
 package readside.application;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import readside.application.api.BookingService;
 import readside.application.dto.BookingDTO;
+import writeside.domain.Booking;
+import readside.domain.api.BookingRepository;
+import writeside.domain.api.RoomRepository;
 
 import java.time.LocalDate;
 import java.util.List;
 
 public class BookingServiceImpl implements BookingService {
 
+    @Autowired
+    private BookingRepository bookingRepository;
+
     @Override
     public List<BookingDTO> getBookings(LocalDate fromDate, LocalDate toDate) {
-        return null;
+
+        List<Booking> bookings = bookingRepository.getBookings(fromDate, toDate);
+        return BookingDTO.fromBooking(bookings);
     }
 }
--- src/main/java/readside/application/dto/BookingDTO.java
+++ src/main/java/readside/application/dto/BookingDTO.java
@@ -1,4 +1,12 @@
 package readside.application.dto;
 
+import writeside.domain.Booking;
+
+import java.util.List;
+
 public class BookingDTO {
+
+    public static List<BookingDTO> fromBooking(List<Booking> bookings) {
+
+    }
 }
